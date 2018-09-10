a10c7ae0e59ffed73a392aa51053c9f8bd713ac1
StephGit <mail@stephangirod.ch>
2018-09-10 21:29:33 +0200

    added screenlayout and keymap

---
+++ link/.screenlayout/internal_off_LVDS-1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --off --output LVDS-1-2 --off --output LVDS-1 --off --output VGA-1-2 --off --output DP-1-3 --off --output DP-1-2 --off --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
