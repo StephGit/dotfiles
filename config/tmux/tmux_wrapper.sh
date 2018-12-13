6151539d799a55e4ccbe0348c1c4777e5f2c5aad
Stephan Girod (extern) <stephan.girod@mobi.ch>
2018-12-13 14:10:20 +0100

    added tmux-wrapper and session

---
+++ config/tmux/tmux_wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sess=session1
+
+# test if the session has windows
+is_closed(){ 
+    n=$(tmux ls 2> /dev/null | grep "^$sess" | wc -l)
+    [[ $n -eq 0 ]]
+}
+
+# either create it or attach to it
+if is_closed ; then
+  tmux source-file ~/.config/tmux/$sess
+  tmux attach -t $sess
+else
+  tmux attach -t $sess
+fi
+
+# the session is now either closed or detatched
+if is_closed ; then
+  tmux kill-session -t $sess
+fi
