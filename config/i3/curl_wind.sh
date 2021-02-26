606394b3ca30fb027bcb998e504b1b58a53d4f7c
StephGit <mail@stephangirod.ch>
2021-02-26 11:31:55 +0100

    some updates

--- config/i3/curl_wind.sh
+++ config/i3/curl_wind.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-PAGE="http://yvbeach.com/yvmeteo.htm"
+YV="http://yvbeach.com/yvmeteo.htm"
+WSCT="http://thunerwetter.ch/wsct_wind.html"
 
-function get_wind_data {
-  LINES=$( curl $PAGE -s | grep -i 'km/h\|DIRECTION' )
-  WIND=$( echo $LINES | grep -oP "\\d*\\.\\d" | tr '\n' ' ' )
-  DEG=$( echo $LINES | grep -oP "\\d*&deg" | grep -oP "\\d*")
+function get_yv_data {
+  local LINES=$( curl $YV -s | grep -i 'km/h\|DIRECTION' )
+  YV_WIND=$( echo $LINES | grep -oP "\\d*\\.\\d" | tr '\n' ' ' )
+  YV_DEG=$( echo $LINES | grep -oP "\\d*&deg" | grep -oP "\\d*")
 }
 
-function get_dir_icon {
+function get_wsct_data {
+  local LINES=$( curl $WSCT -s | grep 'km/h\|aus' )
+  echo $LINES
+  WSCT_WIND=$( echo $LINES | grep -oP "\\d*\\.\\d" | tr '\n' ' ' )
+  WSCT_DEG=$( echo $LINES | grep 'aus' | grep -oP "[A-Z]*")
+}
+
+function get_dir_icon () {
+  local DEG=$1
   if [[ $DEG -gt 339 && $DEG -lt 25 ]]; then
     ICO=↓
   elif [[ $DEG -gt 24 && $DEG -lt 70 ]]; then
@@ -26,13 +35,41 @@ function get_dir_icon {
   elif [[ $DEG -gt 294 && $DEG -lt 340 ]]; then
     ICO=↘
   else
-    ICO=
+    ICO=↓
   fi
 }
 
-get_wind_data
-get_dir_icon
+function get_dir_icon2 () {
+  local DEG=$1
+  if [[ $DEG == "N" ]]; then
+    ICO=↓
+  elif [[ $DEG == "NO" ]]; then
+    ICO=↙
+  elif [[ $DEG == "O" ]]; then
+    ICO=←
+  elif [[ $DEG == "SO" ]]; then
+    ICO=↖
+  elif [[ $DEG == "S" ]]; then
+    ICO=↑
+  elif [[ $DEG == "SW" ]]; then
+    ICO=↗
+  elif [[ $DEG == "W" ]]; then
+    ICO=→
+  elif [[ $DEG == "NW" ]]; then
+    ICO=↘
+  else
+    ICO=↓
+  fi
+}
 
-echo "$WIND$ICO"
+get_yv_data
+get_dir_icon $YV_DEG
+YV_ICO=$ICO
+get_wsct_data
+get_dir_icon2 $WSCT_DEG
+WSCT_ICO=$ICO
+
+echo "YV $YV_WIND$YV_ICO TH $WSCT_WIND$WSCT_ICO"
 
 exit 0
+
